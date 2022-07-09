bc8c5acdabb09f914c594c1b107ff0ff4b53c361
Allan Gadelha <allangadelha@gmail.com>
2022-07-09 19:23:17 -0300

    Criando rotas do sistema: Login e Home

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,25 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import VueRouter from 'vue-router'
 
-createApp(App).use(router).mount('#app')
+import LoginComponent from './pages/Login/LoginComponent'
+import HomeComponent from './pages/Home/HomeComponent'
+
+Vue.config.productionTip = false
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', name:'login', component: LoginComponent },
+  { path: '/login', name:'login', component: LoginComponent },
+  { path: '/home', name:'home', component: HomeComponent }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app')
